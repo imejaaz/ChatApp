fcc92c3073df61ff26b3c76c58f6cb726a181603
Ijaz Ahmad <Ijaz.professional@gmail.com>
2024-05-01 23:53:04 +0500

    c...

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 def index(request):
     return render(request, 'index.html')
 
@@ -9,17 +10,24 @@ def index(request):
 def signup(request):
     if request.method == 'POST':
 
-        email = request.POST.get('email')
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        confirm_password = request.POST.get('confirm_password')
-        print(username)
-        print(email)
-        print(password)
-        user = User.objects.create_user(username, email, password)
+        if 'login_username' in request.POST:
+            username = request.POST.get('login_username')
+            password = request.POST.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+            return render(request, 'homepage.html')
 
-
-        return redirect('home')
+        else:
+            email = request.POST.get('email')
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            confirm_password = request.POST.get('confirm_password')
+            print(username)
+            print(email)
+            print(password)
+            user = User.objects.create_user(username, email, password)
+            return redirect('home')
         
 
     return render(request, 'login.html')
