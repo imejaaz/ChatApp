7c16387b12488822b563c122947b714538297a78
Ijaz Ahmad <Ijaz.professional@gmail.com>
2024-05-14 14:21:10 +0500

    auth done

---
+++ account/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+def index(request):
+    return render(request, 'account/index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+
+        if 'login_username' in request.POST:
+            username = request.POST.get('login_username')
+            password = request.POST.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                print('Login successfully')
+                return redirect('/post/')
+            print('invalid input')
+            return HttpResponse("invalid input")
+
+        if 'email' in request.POST:
+            email = request.POST.get('email')
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            confirm_password = request.POST.get('confirm_password')
+            if password != confirm_password:
+                print('password does not match!')
+                return HttpResponse('password is not same')
+            user = User.objects.create_user(username, email, password)
+            print('Register successfully')
+            return redirect('home')
+        
+
+    return render(request, 'account/login.html')
+    
---
+++ social/asgi.py
@@ -0,0 +1,12 @@
+import os
+from account import routing
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social.settings')
+
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter(routing.websocket_urlpatterns)
+})
